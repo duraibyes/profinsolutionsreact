bc32c7211d31f51c8813d20d79619543e35e690d
duraibyes <duraibytes@gmail.com>
2024-05-30 19:57:30 +0530

    css added

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -63,35 +63,35 @@ const Home = () => {
             Lenders
           </Typography>
           <Box className=" flex w-100 flex-row flex-wrap py-20" sx={{gap: '10px'}}>
-            <Box className="flex w-40">
+            <Box className="flex w-45">
                 <img src={ListIcon} className="pr-10 w-25px" />
                 <Typography> Bussiness Loan</Typography>
             </Box>
-            <Box className="flex w-40">
+            <Box className="flex w-45">
                 <img src={ListIcon} className="pr-10 w-25px" />
                 <Typography> Mortgage Loan</Typography>
             </Box>
-            <Box className="flex w-40">
+            <Box className="flex w-45">
                 <img src={ListIcon} className="pr-10 w-25px" />
                 <Typography> Professional Loan</Typography>
             </Box>
-            <Box className="flex w-40">
+            <Box className="flex w-45">
                 <img src={ListIcon} className="pr-10 w-25px" />
                 <Typography> Medical Equipment Loan</Typography>
             </Box>
-            <Box className="flex w-40">
+            <Box className="flex w-45">
                 <img src={ListIcon} className="pr-10 w-25px" />
                 <Typography> Personal Loan</Typography>
             </Box>
-            <Box className="flex w-40">
+            <Box className="flex w-45">
                 <img src={ListIcon} className="pr-10 w-25px" />
                 <Typography> Industry Machinery Loan</Typography>
             </Box>
-            <Box className="flex w-40">
+            <Box className="flex w-45">
                 <img src={ListIcon} className="pr-10 w-25px" />
                 <Typography> Home Loan</Typography>
             </Box>
-            <Box className="flex w-40">
+            <Box className="flex w-45">
                 <img src={ListIcon} className="pr-10 w-25px" />
                 <Typography> SME Loan</Typography>
             </Box>
